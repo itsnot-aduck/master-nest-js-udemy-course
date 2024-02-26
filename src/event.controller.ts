5abbee63d37d50e8d77beb763a2e6534d9c73d56
itsnot-aduck <hoangduc1662002@gmail.com>
2024-02-26 09:18:20 +0700

    initialize

---
+++ src/event.controller.ts
@@ -0,0 +1,25 @@
+import { Controller, Delete, Get, Patch, Post } from "@nestjs/common";
+
+@Controller('/events')
+export class EventsController{ 
+    @Get()
+    findAll(){
+
+    }
+    @Get()
+    findOne(){
+
+    }
+    @Post()
+    create(){
+
+    }
+    @Patch()
+    update(){
+
+    }
+    @Delete()
+    remove(){
+
+    }
+}
